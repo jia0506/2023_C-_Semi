268c18bcfb240af4af73c3047080acaaf6e0c046
jia0506 <84330129+jia0506@users.noreply.github.com>
2023-03-27 16:14:38 +0900

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -4,8 +4,24 @@ using namespace std;
 int main() {
 	int a, b, c;
 	cin >> a >> b >> c;
+	int money;
 
 	if (a == b && b == c) {
-		10000 + 
+		money = 10000 + a * 1000;
 	}
+	else if ((a == b) || (b == c) || (a == c)) {
+		if (a == b)
+			money = 1000 + a * 100;
+		else
+			money = 1000 + c * 100;
+	}
+	else{
+		if (a > b && a > c)
+			money = a * 100;
+		else if (a < b && b > c)
+			money = b * 100;
+		else
+			money = c * 100;
+	}
+	cout << money;
 }
